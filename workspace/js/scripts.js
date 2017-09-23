22301375d67d75be472cafa3d0ead2b2c34476f3
soroz30 <soroz30@yahoo.pl>
2017-09-23 22:20:38 +0200

    added comments

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,3 +1,12 @@
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+//	alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+/* 
+	var hobby = prompt('What's your hobby?');
+*/
+
+/*
+	second log
+	console.log ('Your hobby is ' + hobby');
+*/
