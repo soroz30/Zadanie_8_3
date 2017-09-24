f1b9a17b9c9b4b98ca0869137f12c57cc4d1f650
soroz30 <soroz30@yahoo.pl>
2017-09-24 11:42:21 +0200

    triangle exercise

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,6 +1,6 @@
-var name = prompt('Enter your name');
-//	alert('Hello, ' + name);
-console.log('Hello, ' + name);
+	var name = prompt('Enter your name');
+	alert('Hello, ' + name);
+//  console.log('Hello, ' + name);
 
 /* 
 	var hobby = prompt('What's your hobby?');
@@ -10,3 +10,9 @@ console.log('Hello, ' + name);
 	second log
 	console.log ('Your hobby is ' + hobby');
 */
+
+var a = prompt('Enter the triangle base');
+	h = prompt('Enter the triangle height');
+	triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
